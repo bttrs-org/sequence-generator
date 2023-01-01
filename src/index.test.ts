33cc5bad0ae52cc5e5b7e2566817e25e6269d5b1
leopold537 <leopolds537@gmail.com>
2023-01-01 18:40:31 +0100

    tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -17,28 +17,37 @@ describe('generator', () => {
         });
     });
 
+    test('options - shuffled alphabet', () => {
+        const gen = createGenerator({
+            alphabet: '01234',
+            shuffle: true,
+        });
+        expect(gen.options.alphabet).not.toBe('01234');
+        expect(gen.options.alphabet.split('').sort().join('')).toBe('01234');
+    });
+
     test('options - correct minLength and maxLength calculated', () => {
         let gen = createGenerator({
             alphabet: '01234',
             min: 0,
             max: 5,
         });
-        expect(gen.options.minLength).toEqual(1);
-        expect(gen.options.maxLength).toEqual(2);
+        expect(gen.options.minLength).toBe(1);
+        expect(gen.options.maxLength).toBe(2);
         gen = createGenerator({
             alphabet: '01234',
             min: 4,
             max: 6,
         });
-        expect(gen.options.minLength).toEqual(1);
-        expect(gen.options.maxLength).toEqual(2);
+        expect(gen.options.minLength).toBe(1);
+        expect(gen.options.maxLength).toBe(2);
         gen = createGenerator({
             alphabet: '01234',
             min: 24,
             max: 25,
         });
-        expect(gen.options.minLength).toEqual(2);
-        expect(gen.options.maxLength).toEqual(3);
+        expect(gen.options.minLength).toBe(2);
+        expect(gen.options.maxLength).toBe(3);
     });
 
     test('options - correct min and max calculated', () => {
@@ -47,16 +56,16 @@ describe('generator', () => {
             minLength: 1,
             maxLength: 1,
         });
-        expect(gen.options.min).toEqual(0);
-        expect(gen.options.max).toEqual(4);
+        expect(gen.options.min).toBe(0);
+        expect(gen.options.max).toBe(4);
 
         gen = createGenerator({
             alphabet: '01234',
             minLength: 2,
             maxLength: 2,
         });
-        expect(gen.options.min).toEqual(5);
-        expect(gen.options.max).toEqual(24);
+        expect(gen.options.min).toBe(5);
+        expect(gen.options.max).toBe(24);
 
         gen = createGenerator({
             alphabet: '01234',
@@ -65,8 +74,8 @@ describe('generator', () => {
             minLength: 2,
             maxLength: 3,
         });
-        expect(gen.options.min).toEqual(5);
-        expect(gen.options.max).toEqual(124);
+        expect(gen.options.min).toBe(5);
+        expect(gen.options.max).toBe(124);
     });
 
     test('generate values', () => {
@@ -108,5 +117,39 @@ describe('generator', () => {
             values.push(gen());
         }
         expect(values).toEqual(['0', '1', '2', '3', '4', '10', '0', '1', '2', '3', '4', '10']);
+    });
+
+    test('generate shuffled values', () => {
+        let gen: Generator;
+        let values: string[];
+
+        gen = createGenerator({
+            alphabet: '01234',
+            shuffle: true,
+            max: 4,
+        });
+        values = [];
+        for (let i = 0; i < 5; i++) {
+            values.push(gen());
+        }
+        expect(values).not.toEqual(['0', '1', '2', '3', '4']);
+        expect(values.sort()).toEqual(['0', '1', '2', '3', '4']);
+    });
+
+    test('generate random values', () => {
+        let gen: Generator;
+        let values: string[];
+
+        gen = createGenerator({
+            alphabet: '01234',
+            random: true,
+            max: 10,
+        });
+        values = [];
+        for (let i = 0; i < 12; i++) {
+            values.push(gen());
+        }
+        expect(values).not.toEqual(['0', '1', '2', '3', '4', '10', '11', '12', '13', '14', '20', '0']);
+        expect(values[0]).toBe(values[values.length - 1]);
     });
 });
